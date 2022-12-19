36dff93196c59d324ae2ed890d5fe2e999038153
Patryk Lach <patryk.lach@mp.pl>
2022-12-19 09:23:58 +0100

    fix(Zadanie14, Zadanie8): exercise fix

--- Zadania/Zadanie8.c
+++ Zadania/Zadanie8.c
@@ -3,10 +3,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct RZYM { int arabian; char *roman; } rz[]={{1,"I"},{4,"IV"},{5,"V"},{9,"IX"},{10,"X"}, {40,"XL"},{50,"L"},{90,"XC"},{100,"C"}, {400,"CD"},{500,"D"},{900,"CM"},{1000,"M"}};
+
 int main ()
 {
-    struct RZYM { int arabian; char *roman; } rz[]={{1,"I"},{4,"IV"},{5,"V"},{9,"IX"},{10,"X"}, {40,"XL"},{50,"L"},{90,"XC"},{100,"C"}, {400,"CD"},{500,"D"},{900,"CM"},{1000,"M"}};
-
     char a[4000];
     printf("Kalkulator liczb Rzymskich <=> Arabskich\nPodaj liczbę: ");
     scanf("%s", a);
@@ -23,15 +23,18 @@ int main ()
     } else {
         int sum = 0;
         for (int i = 0; i < strlen(a); i++) {
+            int isFound = 0;
             for (int k = 0; k < 13; k++) {
                 if (a[i] == rz[k].roman[0] && a[i+1] == rz[k].roman[1]) {
                     sum += rz[k].arabian;
-                    i+=2;
+                    isFound = 1;
+                    i++;
                     break;
                 }
             }
+            if (isFound == 1) { continue; }
             for (int k = 0; k < 13; k++) {
-                if (a[i] == rz[k].roman[0] && a[i+1] != rz[k].roman[1]) {
+                if (a[i] == rz[k].roman[0]) {
                     sum += rz[k].arabian;
                     break;
                 }
